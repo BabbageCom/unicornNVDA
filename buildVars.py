eb8e6618425cc8fa7f81d0b91db2014789036866
Leonard de Ruijter <leonard@babbage.com>
2017-04-10 16:18:55 +0200

    Added babbage to buildvars and make our version a + version

--- buildVars.py
+++ buildVars.py
@@ -19,9 +19,9 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Allows remote control of and remote access to another machine."""),
 	# version
-	"addon_version" : "2.0",
+	"addon_version" : "2.0+",
 	# Author(s)
-	"addon_author" : u"Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>",
+	"addon_author" : u"Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>, Babbage B.V. ,info@babbage.com>",
 	# URL for the add-on documentation support
 	"addon_url" : "http://NVDARemote.com",
 	# Documentation file name
