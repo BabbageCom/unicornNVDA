b9e06faca564c206ed5e4fce5d9c557f4c60c0ec
Leonard de Ruijter <leonard@babbage.com>
2017-10-03 13:19:58 +0200

    Fix onReceive

--- addon/globalPlugins/remoteClient/transport.py
+++ addon/globalPlugins/remoteClient/transport.py
@@ -305,11 +305,12 @@ class DVCTransport(Transport,UnicornCallbackHandler):
 		return 0
 
 	def _OnDataReceived(self,cbSize,pBuffer):
-		pBuffer=cast(pBuffer,c_wchar_p)
-		str=pBuffer.value
-		pBufferSize=(len(str)+1)*sizeof(c_wchar)
-		assert pBufferSize==cbSize, "size of pBuffer %d not equal to cbSize %d"%(pBufferSize,cbSize)
-		self.handle_data(str)
+		pBuffer=cast(pBuffer,POINTER(c_wchar*(cbSize/sizeof(c_wchar))))
+		str=u"".join(pBuffer.contents)
+		if u"\x00" not in str:
+			self.buffer+=str
+		else:
+			self.handle_data(str.replace(u"\x00",u""))
 		return 0
 
 	def _OnReadError(self,dwError):
