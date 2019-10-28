ea4404bc14973656608a517dc06700d72eb2ca6e
Leonard de Ruijter <leonard@babbage.com>
2019-10-28 11:01:57 +0100

    Remove tones and wave fro msessions

--- addon/globalPlugins/unicorn/session.py
+++ addon/globalPlugins/unicorn/session.py
@@ -19,42 +19,14 @@ class RemoteSession(object):
 		self.local_machine = local_machine
 		self.patcher = None
 		self.transport = transport
-		self.transport.callback_manager.register_callback('msg_version_mismatch', self.handle_version_mismatch)
-		self.transport.callback_manager.register_callback('msg_motd', self.handle_motd)
-
-	def handle_version_mismatch(self, **kwargs):
-		#translators: Message for version mismatch
-		message = _("""The version of the relay server which you have connected to is not compatible with this version of the Remote Client.
-Please either use a different server or upgrade your version of the addon.""")
-		ui.message(message)
-		self.transport.close()
-
-	def handle_motd(self, motd, force_display=False, **kwargs):
-		if force_display or self.should_display_motd(motd):
-			gui.messageBox(parent=gui.mainFrame, caption=_("Message of the Day"), message=motd)
-
-	def should_display_motd(self, motd):
-		conf = configuration.get_config()
-		host, port = self.transport.address
-		host = host.lower()
-		address = '{host}:{port}'.format(host=host, port=port)
-		hashed = hashlib.sha1(motd).hexdigest()
-		current = conf['seen_motds'].get(address, "")
-		if current == hashed:
-			return False
-		conf['seen_motds'][address] = hashed
-		conf.write()
-		return True
 
 class SlaveSession(RemoteSession):	
 	"""Session that runs on the slave and manages state."""
 
-	def __init__(self, *args, **kwargs):
-		is_secondary = kwargs.pop("is_secondary",False)
+	def __init__(self, *args, is_secondary=False, **kwargs):
 		super(SlaveSession, self).__init__(*args, **kwargs)
 		self.transport.callback_manager.register_callback('msg_client_joined', self.handle_client_connected)
 		self.transport.callback_manager.register_callback('msg_client_left', self.handle_client_disconnected)
-		self.transport.callback_manager.register_callback('msg_key', self.local_machine.send_key)
 		self.masters = defaultdict(dict)
 		self.master_display_sizes=[]
 		self.last_client_index = None
@@ -64,16 +36,9 @@ class SlaveSession(RemoteSession):
 		self.patcher = nvda_patcher.NVDASlavePatcher(is_secondary=is_secondary)
 		self.patch_callbacks_added = False
 		self.transport.callback_manager.register_callback('msg_channel_joined', self.handle_channel_joined)
-		self.transport.callback_manager.register_callback('msg_set_clipboard_text', self.local_machine.set_clipboard_text)
 		self.transport.callback_manager.register_callback('msg_set_braille_info', self.handle_braille_info)
 		self.transport.callback_manager.register_callback('msg_set_display_size', self.set_display_size)
 		self.transport.callback_manager.register_callback('msg_braille_input', self.local_machine.braille_input)
-		self.transport.callback_manager.register_callback('msg_send_SAS', self.local_machine.send_SAS)
-
-	def get_connection_info(self):
-		hostname, port = self.transport.address
-		key = self.transport.channel
-		return connection_info.ConnectionInfo(hostname=hostname, port=port, key=key, mode='slave')
 
 	def handle_client_connected(self, client=None, **kwargs):
 		self.patcher.patch()
@@ -137,12 +102,6 @@ class SlaveSession(RemoteSession):
 	def _get_lastIndex(self):
 		return self.last_client_index
 
-	def beep(self, hz, length, left=50, right=50):
-		self.transport.send(type='tone', hz=hz, length=length, left=left, right=right)
-
-	def playWaveFile(self, fileName, async=True):
-		self.transport.send(type='wave', fileName=fileName, async=async)
-
 	def display(self, cells):
 		# Only send braille data when there are controlling machines with a braille display
 		if self.has_braille_masters():
@@ -164,27 +123,13 @@ class MasterSession(RemoteSession):
 		self.patch_callbacks_added = False
 		self.transport.callback_manager.register_callback('msg_speak', self.local_machine.speak)
 		self.transport.callback_manager.register_callback('msg_cancel', self.local_machine.cancel_speech)
-		self.transport.callback_manager.register_callback('msg_tone', self.local_machine.beep)
-		self.transport.callback_manager.register_callback('msg_wave', self.local_machine.play_wave)
 		self.transport.callback_manager.register_callback('msg_display', self.local_machine.display)
-		self.transport.callback_manager.register_callback('msg_nvda_not_connected', self.handle_nvda_not_connected)
 		self.transport.callback_manager.register_callback('msg_client_joined', self.handle_client_connected)
 		self.transport.callback_manager.register_callback('msg_client_left', self.handle_client_disconnected)
 		self.transport.callback_manager.register_callback('msg_channel_joined', self.handle_channel_joined)
-		self.transport.callback_manager.register_callback('msg_set_clipboard_text', self.local_machine.set_clipboard_text)
 		self.transport.callback_manager.register_callback('msg_send_braille_info', self.send_braille_info)
 		self.transport.callback_manager.register_callback('transport_connected', self.handle_connected)
 		self.transport.callback_manager.register_callback('transport_disconnected', self.handle_disconnected)
-
-
-	def get_connection_info(self):
-		hostname, port = self.transport.address
-		key = self.transport.channel
-		return connection_info.ConnectionInfo(hostname=hostname, port=port, key=key, mode='master')
-
-	def handle_nvda_not_connected(self):
-		speech.cancelSpeech()
-		ui.message(_("Remote NVDA not connected."))
 
 	def handle_connected(self):
 		if self.index_thread is not None:
