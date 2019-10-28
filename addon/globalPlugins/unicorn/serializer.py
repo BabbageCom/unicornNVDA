2a07d3372a84d3ef2342d73faf2169d6e8036e2a
Leonard de Ruijter <leonard@babbage.com>
2019-10-28 15:09:04 +0100

    Python 3 serializer: support speech framework

--- addon/globalPlugins/unicorn/serializer.py
+++ addon/globalPlugins/unicorn/serializer.py
@@ -1,14 +1,7 @@
-from logging import getLogger
-log = getLogger('serializer')
 import sys
 import os
-try:
-	import json
-except ImportError:
-	sys.path.append(os.path.abspath(os.path.dirname(__file__)))
-	import json
-	sys.path.remove(sys.path[-1])
-import speech
+import json
+import speech.commands
 
 class JSONSerializer:
 	SEP = '\n'
@@ -23,7 +16,12 @@ class JSONSerializer:
 		return obj
 
 
-SEQUENCE_CLASSES = (speech.SpeechCommand, speech.CharacterModeCommand, speech.BreakCommand)
+SEQUENCE_CLASSES = (
+	speech.commands.SynthCommand,
+	speech.commands.EndUtteranceCommand,
+	speech.commands.BeepCommand,
+)
+
 class CustomEncoder(json.JSONEncoder):
 
 	def default(self, obj):
@@ -46,7 +44,7 @@ def as_sequence(dct):
 			sequence.append(item)
 			continue
 		name, values = item
-		if not hasattr(speech, name):
+		if not hasattr(speech.commands, name):
 			log.warning("Unknown sequence type received: %r" % name)
 			continue
 		cls = getattr(speech, name)
