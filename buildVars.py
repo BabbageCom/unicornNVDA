4bb13d2623035a0a8a80d60c6182e91b6ebed3f7
Leonard de Ruijter <leonard@babbage.com>
2019-10-28 08:43:04 +0100

    Stripping unneeded stuff

--- buildVars.py
+++ buildVars.py
@@ -11,32 +11,35 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
-	"addon_name" : "remote",
+	"addon_name" : "unicorn",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("Remote Support"),
+	"addon_summary" : _("UnicornDVC integration for NVDA"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Allows remote control of and remote access to another machine."""),
+	"addon_description" : _("Allows sending speech and braille over RDP, Citrix and VMWare Horizon virtual channels using Access4U UnicornDVC."),
 	# version
-	"addon_version" : "2.2+",
+	"addon_version" : "3.0",
 	# Author(s)
-	"addon_author" : u"Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>, Babbage B.V. <info@babbage.com>",
+	"addon_author" : "Babbage B.V. <info@babbage.com>, NVDA Remote contributors",
 	# URL for the add-on documentation support
-	"addon_url" : "http://NVDARemote.com",
+	"addon_url" : "https://access4u.eu/",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion" : "2019.3",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2019.3",
+
 }
 
 
-import os.path
+import os.paths
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
 pythonSources = [
 	'addon/globalPlugins/*/*.py',
-	'addon/globalPlugins/*/*.exe',
-	'addon/synthDrivers/*/*.py',
 ]
 
 # Files that contain strings for translation. Usually your python sources
@@ -44,4 +47,4 @@ i18nSources = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
-excludedFiles = ['globalPlugins\\remoteClient\\url_handler.obj']
+excludedFiles = []
